32232c980ae0c76096aa0ca1fa7e899f05135f80
VVNTHEFLASH <vishnurockhunters@gmail.com>
2021-10-03 13:06:46 +0530

    Superwars Stage 4 Done

--- src/app.js
+++ src/app.js
@@ -2,23 +2,23 @@ const PLAYERS = [
     "Spiderman",
     "Captain America",
     "Wonderwoman",
-    // "Popcorn",
-    // "Gemwoman",
-    // "Bolt",
-    // "Antwoman",
-    // "Mask",
-    // "Tiger",
-    // "Captain",
-    // "Catwoman",
-    // "Fish",
-    // "Hulk",
-    // "Ninja",
-    // "Black Cat",
-    // "Volverine",
-    // "Thor",
-    // "Slayer",
-    // "Vader",
-    // "Slingo"
+    "Popcorn",
+    "Gemwoman",
+    "Bolt",
+    "Antwoman",
+    "Mask",
+    "Tiger",
+    "Captain",
+    "Catwoman",
+    "Fish",
+    "Hulk",
+    "Ninja",
+    "Black Cat",
+    "Volverine",
+    "Thor",
+    "Slayer",
+    "Vader",
+    "Slingo"
 ];
 
 // Player Class
@@ -26,8 +26,14 @@ class Player {
     constructor(id, name, type) {
         // Create member variables and assign values
         // Type your code
-
+        this.id = id;
+        this.name = name;
+        this.strength = this.getRandomStrength();
+        this.image = 'images/super-'+(id+1)+'.png';
+        this.type = type;
     }
+    
+
 
     // getting random strength
     getRandomStrength = () => {
@@ -38,8 +44,22 @@ class Player {
     view = () => {
         // Accumulate HTML template
         // Type your code here
+        let player = document.createElement('div');
+        player.classList.add('player');
+        player.setAttribute('data-id', this.id);
+        // create img element and set attribute src as this.image
+        let img = document.createElement('img');
+        img.setAttribute('src',this.image);
+        // create div element for name
+        let name = document.createElement('div');
+        name.textContent = this.name;
+        // create div element for strenth
+        let strength = document.createElement('div');
+        strength.classList = 'strength';
+        strength.textContent = this.strength;
+        //append img,name and strength to players
+        player.append(img, name, strength);
 
-    
         return player;
     }
 }
@@ -50,7 +70,10 @@ class Superwar {
     // Create a field players 
     // Use Map method to loop through players argument and create new players
     // Type your code here
-
+        this.players = players.map((player, num) => {
+            let type = (num % 2 == 0) ? 'hero' : 'villain';
+            return new Player(num, player, type)
+        });
     }
 
     // Display players in HTML
